62dcc43983e4303fa7b94df7b091790a6f23e0dd
Maks <Crossfade143@gmail.com>
2023-08-03 23:00:45 +0300

    done configure gulp for JS files

--- gulp/configs/path.js
+++ gulp/configs/path.js
@@ -7,16 +7,19 @@ const srcFolder = "./src";
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   watch: {
+    js: `${srcFolder}/js/**/*.js`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`,
--- gulpfile.js
+++ gulpfile.js
@@ -18,15 +18,17 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
+import { scriptJS } from "./gulp/tasks/js.js";
 
 //watcher
 function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, scriptJS);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss);
+const mainTasks = gulp.parallel(copy, scriptJS, html, scss);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
