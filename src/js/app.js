3ba0f63b413ef80735a46aace3619a840a01c0a1
Maks <Crossfade143@gmail.com>
2023-08-09 20:14:07 +0300

    done section header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const headerMenu = document.querySelector("#menu-icon");
+const headerMenuIcon = document.querySelector(".fa-bars");
+const navbar = document.querySelector(".navbar");
+
+console.log(headerMenuIcon);
+
+headerMenu.addEventListener("click", (e) => {
+  navbar.classList.toggle("open");
+  headerMenuIcon.classList.toggle("fa-bars");
+  headerMenuIcon.classList.toggle("fa-xmark");
+});
