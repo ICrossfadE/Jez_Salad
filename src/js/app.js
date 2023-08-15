b101f5b148cb48427b050de1f9fc610ea911f8ac
Maks <Crossfade143@gmail.com>
2023-08-15 17:09:30 +0300

    develop slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,49 @@
 import * as flsFunctions from "./modules/functions.js";
 
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
 flsFunctions.isWebp();
 
+new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+
+  //Aroows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+
+  // simulateTouch: true,
+  slidesPerView: 3,
+
+  loop: true,
+
+  spaceBetween: 30,
+
+  breakpoints: {
+    1440: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    425: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+//Header menu
 const headerMenu = document.querySelector("#menu-icon");
 const headerMenuIcon = document.querySelector(".fa-bars");
 const navbar = document.querySelector(".navbar");
 const header = document.querySelector(".header__container");
-// const taskButton = document.querySelectorAll(".task-button");
-
-// console.log(taskButton);
-
-// taskButton.addEventListener("mouseenter", (e) => {
-//   console.log("enter");
-// });
 
 headerMenu.addEventListener("click", (e) => {
   e.preventDefault();
