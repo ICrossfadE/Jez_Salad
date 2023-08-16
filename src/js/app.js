c356959d69c0fcadc7b0e93d2cf5c8ade49724fe
Maks <Crossfade143@gmail.com>
2023-08-17 00:18:54 +0300

    dvelop slider fraction

--- src/js/app.js
+++ src/js/app.js
@@ -44,6 +44,23 @@ const headerMenu = document.querySelector("#menu-icon");
 const headerMenuIcon = document.querySelector(".fa-bars");
 const navbar = document.querySelector(".navbar");
 const header = document.querySelector(".header__container");
+const currentClass = document.querySelector(".swiper-pagination-current");
+const totalClass = document.querySelector(".swiper-pagination-total");
+const buttonNext = document.querySelector(".swiper-button-next");
+const buttonPrev = document.querySelector(".swiper-button-prev");
+
+function renderFraction(current, total) {
+  const zeroLength = 2;
+  const currentText = current.textContent;
+  const totalText = total.textContent;
+
+  return (
+    (current.textContent = `${currentText.padStart(zeroLength, "0")}`),
+    (total.textContent = `${totalText.padStart(zeroLength, "0")}`)
+  );
+}
+
+renderFraction(currentClass, totalClass);
 
 headerMenu.addEventListener("click", (e) => {
   e.preventDefault();
@@ -57,4 +74,12 @@ header.addEventListener("click", (e) => {
 });
 navbar.addEventListener("click", (e) => {
   e.preventDefault();
+});
+buttonNext.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderFraction(currentClass, totalClass);
+});
+buttonPrev.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderFraction(currentClass, totalClass);
 });
